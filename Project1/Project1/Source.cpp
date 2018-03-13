d226bd979bc8c7bd2290cfb1ef8ea0e06abcac84
danieljesus0995 <35454827+danieljesus0995@users.noreply.github.com>
2018-03-13 16:11:19 +0000

    Initial commit

---
+++ Project1/Project1/Source.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int opcao;
+		cout << "\n\nExercicios C/C++" << endl;
+		cout << " 1 - Divisao Inteira \n";
+		cout << " 2 - Valores por omissao \n";
+		cout << " 3 - Vetor \n";
+		cout << " 0 - Sair \n";
+		cout << " Opcao > ";
+		std::cin >> opcao;
+}
